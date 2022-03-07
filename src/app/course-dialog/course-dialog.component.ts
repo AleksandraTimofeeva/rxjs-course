17c4a9d5f648ee435fc5a6268646831fdcf075c0
atimofeeva <welcomeToApt1>
2022-03-07 22:06:25 +0100

    Add form draft pre-save example and rxjs filter operator

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -13,24 +13,20 @@ import {Store} from '../common/store.service';
     templateUrl: './course-dialog.component.html',
     styleUrls: ['./course-dialog.component.css']
 })
-export class CourseDialogComponent implements AfterViewInit {
+export class CourseDialogComponent implements OnInit {
 
     form: FormGroup;
-
     course:Course;
 
     @ViewChild('saveButton', { static: true }) saveButton: ElementRef;
-
     @ViewChild('searchInput', { static: true }) searchInput : ElementRef;
 
     constructor(
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
         @Inject(MAT_DIALOG_DATA) course:Course,
-        private store:Store) {
-
+        ) {
         this.course = course;
-
         this.form = fb.group({
             description: [course.description, Validators.required],
             category: [course.category, Validators.required],
@@ -40,24 +36,38 @@ export class CourseDialogComponent implements AfterViewInit {
 
     }
 
+    ngOnInit() {
+      this.form.valueChanges
+        .pipe(
+          filter(() => this.form.valid)
+        )
+        .subscribe(changes => {
+          const saveCourse$ = fromPromise(fetch(`/api/courses/${this.course.id}`,{
+              method: 'PUT',
+              body: JSON.stringify(changes),
+              headers: {
+                'content-type': 'application/json'
+              }
+            }));
+        }
+      )
+
+    }
+
     ngAfterViewInit() {
 
     }
 
-    save() {
-        this.store.saveCourse(this.course.id, this.form.value)
-            .subscribe(
-                () => this.close(),
-                err => console.log("Error saving course", err)
-            );
-    }
-
-
+    // save() {
+    //     this.store.saveCourse(this.course.id, this.form.value)
+    //         .subscribe(
+    //             () => this.close(),
+    //             err => console.log("Error saving course", err)
+    //         );
+    // }
 
 
     close() {
         this.dialogRef.close();
     }
-
-
 }
